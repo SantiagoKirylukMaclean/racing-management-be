d394361c3ddb4b73bcaa84fd9966a407059e7636
Santiago Kiryluk Maclean <santiago.kiryluk@gmail.com>
2024-07-01 15:12:24 +0200

    Initial commit with empty application.

---
+++ src/test/kotlin/com/bbqsoft/teamsmanagement/TeamsmanagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bbqsoft.teamsmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamsmanagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
